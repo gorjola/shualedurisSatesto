319299faf104f846a6f0c27cae0d31fbb9ccdc7b
Goga <admin@Gogas-MacBook.local>
2025-11-21 16:46:00 +0400

    Added  view students func

--- Student/main.py
+++ Student/main.py
@@ -1,9 +1,13 @@
+import json
+
+
 class Student:
-    def __init__(self, name="", age=0, grade=""):
+    def __init__(self, name="", age=0, grade=None):
         self.name = name
         self.age = age
-        self.grade = grade
+        self.grade = [grade] if grade is not None else []
 
+    # გადაყავს ქულები ასოებად
     def number_to_letter(self, score):
         if score >= 90:
             return "A"
@@ -16,18 +20,33 @@ class Student:
         else:
             return "F"
 
+    # ახალი სტუდენტის დამატება
     def new_student(self):
         pass
 
+    # ყველა სტუდენტის ნახვა
     def view_students(self):
-        pass
+        with open("Student/students.json", "r") as file:
+            data = json.load(file)
+            for student_id, info in data["students"].items():
+                name = str(info["name"])
+                age = str(info["age"])
+                grade = str(info["grade"])
+                student_id_str = str(student_id)
 
+                print(
+                    f"ID: {student_id_str}, სახელი: {name}, ასაკი: {age}, ქულა: {grade}"
+                )
+
+    # სტუდენტის ძებნა id მიხედვით
     def find_student(self):
         pass
 
+    # მოსწავლის შეფასების განახლება
     def update_grade(self):
         pass
 
+    # მენიუს ჩვენება
     def menu(self):
         display_menu = {
             "1": "ახალი სტუდენტის დამატება",
@@ -51,7 +70,7 @@ class Student:
             if choice == "1":
                 pass
             elif choice == "2":
-                pass
+                self.view_students()
             elif choice == "3":
                 pass
             elif choice == "4":
